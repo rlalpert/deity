4a6fe3d1dedd9a1efb9d1c19f289d625ad53f17a
Rachel Alpert <ambitious.fishus@gmail.com>
2018-02-28 18:24:31 -0500

    grabbed more json info from steam

--- achievements.py
+++ achievements.py
@@ -1,12 +1,21 @@
-import requests
-import os
+# import requests
+# import os
 import json
 
-dev_key = os.environ["STEAM_API_KEY"]
-civ_vi_id = "289070"
-url = "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid={game_id}&format=json".format(game_id=civ_vi_id)
+# dev_key = os.environ["STEAM_API_KEY"]
+# civ_vi_id = "289070"
+# url = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key={key}&appid={appid}".format(key=dev_key, appid=civ_vi_id)
 
-stats = requests.get(url)
+# civ_vi_info = requests.get(url)
+# data = civ_vi_info.json()
 
-with open('achievements_raw.json', 'w') as file:
-    json.dump(stats.json(), file, sort_keys=True, indent=4)
+# with open("achievements_full.json", "w") as f:
+#     json.dump(data["game"]["availableGameStats"]["achievements"], f, sort_keys=True, indent=4)
+
+with open("achievements_raw.json", "r") as f:
+    data = json.load(f)
+
+percentages = data["achievementpercentages"]["achievements"]
+
+with open('achievements_percentages.json', 'w') as f:
+    json.dump(percentages, f, sort_keys=True, indent=4)
