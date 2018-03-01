05bb002c5aec763869ecb737fb82f50e71959c75
Rachel Alpert <ambitious.fishus@gmail.com>
2018-03-01 15:54:30 -0500

    added cs-la_venta, district-holy_site, and building-temple keywords

--- achievements.py
+++ achievements.py
@@ -8,8 +8,8 @@ with open("achievements_keywords.json", "r") as f:
     items = json.load(f)
 
 for item in items:
-    if "hattusa" in item["description"].lower():
-        item.setdefault("keywords", []).append("cs-hattusa")
+    if "temple" in item["description"].lower():
+        item.setdefault("keywords", []).append("building-temple")
 
 with open("achievements_keywords.json", "w") as f:
     json.dump(items, f, sort_keys=True, indent=4)
