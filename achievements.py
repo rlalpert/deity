65cf8977fa4a0c734e5d06f08778a73863206dce
Rachel Alpert <ambitious.fishus@gmail.com>
2018-03-01 14:58:01 -0500

    starting to add keywords

--- achievements.py
+++ achievements.py
@@ -1,21 +1,15 @@
-# import requests
-# import os
 import json
 
 # dev_key = os.environ["STEAM_API_KEY"]
 # civ_vi_id = "289070"
-# url = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key={key}&appid={appid}".format(key=dev_key, appid=civ_vi_id)
 
-# civ_vi_info = requests.get(url)
-# data = civ_vi_info.json()
 
-# with open("achievements_full.json", "w") as f:
-#     json.dump(data["game"]["availableGameStats"]["achievements"], f, sort_keys=True, indent=4)
+with open("achievements_full.json", "r") as f:
+    items = json.load(f)
 
-with open("achievements_raw.json", "r") as f:
-    data = json.load(f)
+for item in items:
+    if "difficult" in item["description"]:
+        item.setdefault("keywords", []).append("difficulty")
 
-percentages = data["achievementpercentages"]["achievements"]
-
-with open('achievements_percentages.json', 'w') as f:
-    json.dump(percentages, f, sort_keys=True, indent=4)
+with open("achievements_keywords.json", "w") as f:
+    json.dump(items, f, sort_keys=True, indent=4)
