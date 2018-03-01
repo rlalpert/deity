e4c5a6d6a882539c930ff51dc9a073f2b4822549
Rachel Alpert <ambitious.fishus@gmail.com>
2018-03-01 15:10:14 -0500

    added victory-science keyword

--- achievements.py
+++ achievements.py
@@ -8,8 +8,8 @@ with open("achievements_keywords.json", "r") as f:
     items = json.load(f)
 
 for item in items:
-    if ("difficulty" and "deity") in item["description"].lower():
-        item.setdefault("keywords", []).append("difficulty-deity")
+    if ("science" and "victory") in item["description"].lower():
+        item.setdefault("keywords", []).append("victory-science")
 
 with open("achievements_keywords.json", "w") as f:
     json.dump(items, f, sort_keys=True, indent=4)
