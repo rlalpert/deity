e6ca2017d524788f57032eecc385f91d422584df
Rachel Alpert <ambitious.fishus@gmail.com>
2018-03-05 14:45:29 -0500

    working on testing out db

--- db.py
+++ db.py
@@ -1,16 +1,17 @@
 import sqlite3
 import json
 from cmd import Cmd 
+import pprint
 
-conn = sqlite3.connect(':memory:')
+# conn = sqlite3.connect(':memory:')
 
-conn.execute("""
-    create table achievements (
-        name text,
-        description text,
-        keywords text
-    );
-    """)
+# conn.execute("""
+#     create table achievements (
+#         name text,
+#         description text,
+#         keywords text
+#     );
+#     """)
 
 with open("achievements_keywords.json", "r") as f:
     achievements_raw = json.load(f)
@@ -18,9 +19,8 @@ with open("achievements_keywords.json", "r") as f:
 achievements = []
 
 for achievement in achievements_raw:
-    dict(name=achievement["displayName"], 
+    achievements.append(dict(name=achievement["displayName"], 
          description=achievement["description"],
-         keywords=achievement["keywords"])
+         keywords=achievement["keywords"]))
 
-for a in achievements:
-    print(a)
+pprint.pprint(achievements)
